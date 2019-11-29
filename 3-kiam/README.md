5e0ef875b8c61dd1974801f00fce6496c6401c3e
Marcel Jepma <mjepma@xebia.com>
2019-11-29 16:56:19 +0100

    WIP

--- 3-kiam/README.md
+++ 3-kiam/README.md
@@ -12,7 +12,7 @@ popd
 ### CA Issuer Certs
 
 ```
-aws-vault exec --no-session --assume-role-ttl=60m xebia-eks -- kubectl apply -f deployments/ca-issuer-certs.yaml
+kubectl apply -f deployments/ca-issuer-certs.yaml
 ```
 
 ## Server
@@ -20,13 +20,13 @@ aws-vault exec --no-session --assume-role-ttl=60m xebia-eks -- kubectl apply -f 
 The server needs a RBAC setup and service account to function properly. Also, we need a CA to generate a certificate that is used to encrypt traffic between KIAM agent and server.
 
 ```
-aws-vault exec --no-session --assume-role-ttl=60m xebia-eks -- kubectl apply -f deployments/server-rbac.yaml -f deployments/server.yaml
+kubectl apply -f deployments/server-rbac.yaml -f deployments/server.yaml
 ```
 
 ## Agent
 
 ```
-aws-vault exec --no-session --assume-role-ttl=60m xebia-eks -- kubectl apply -f deployments/agent.yaml
+kubectl apply -f deployments/agent.yaml
 ```
 
 > *Important:* Make sure you use the proper ENI for the agent. For EKS `!eth0` should be good enough, as all traffic on other NICS will be intercepted (if they are for the metadata service). The complete list can be found here: https://github.com/uswitch/kiam/tree/master#typical-cni-interface-names
--- 3-kiam/terraform/main.tf
+++ 3-kiam/terraform/main.tf
@@ -12,11 +12,6 @@ locals {
   cluster_name = "spark-eks"
 }
 
-# used `_node` in it's name, because this is also used within the default policy names.
-data "aws_iam_role" "kiam_node" {
-  name = "${local.cluster_name}_kiam_node"
-}
-
 resource "aws_iam_role" "kiam_server" {
   name        = "kiam-server"
   description = "Role the Kiam Server process assumes"
@@ -29,32 +24,12 @@ resource "aws_iam_role" "kiam_server" {
       "Sid": "",
       "Effect": "Allow",
       "Principal": {
-        "AWS": "${data.aws_iam_role.kiam_node.arn}"
+        "AWS": "${data.aws_iam_role.server_node.arn}"
       },
       "Action": "sts:AssumeRole"
     }
   ]
 }
-EOF
-}
-
-resource "aws_iam_role_policy" "kiam_node" {
-  name = "kiam-node"
-  role = "${data.aws_iam_role.kiam_node.name}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "sts:AssumeRole"
-      ],
-      "Resource": "${aws_iam_role.kiam_server.arn}"
-    }
-    ]
-  }
 EOF
 }
 
@@ -82,4 +57,28 @@ resource "aws_iam_policy_attachment" "kiam_server" {
   name       = "kiam-server-policy"
   roles      = ["${aws_iam_role.kiam_server.name}"]
   policy_arn = "${aws_iam_policy.kiam_server.arn}"
+}
+
+# used `_node` in it's name, because this is also used within the default policy names.
+data "aws_iam_role" "server_node" {
+  name = "${local.cluster_name}_kiam_node"
+}
+resource "aws_iam_role_policy" "server_node" {
+  name = "kiam-server-node"
+  role = "${data.aws_iam_role.server_node.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "sts:AssumeRole"
+      ],
+      "Resource": "${aws_iam_role.kiam_server.arn}"
+    }
+    ]
+  }
+EOF
 }
